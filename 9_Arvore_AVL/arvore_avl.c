124854ec8b1ee438d151a1f9f7f271d296468dec
Yida <andressayida@yahoo.com.br>
2024-06-11 21:14:14 -0300

    Árvore AVL

--- 9_Arvore_AVL/arvore_avl.c
+++ 9_Arvore_AVL/arvore_avl.c
@@ -45,6 +45,7 @@ int estaVazia(Arvore *raiz){
     return 0;
 }
 
+
 //-----Funções auxiliares----
 
 // retorna a altura do nó
@@ -136,6 +137,198 @@ void posOrdem(Arvore *raiz){
         posOrdem(&((*raiz)->dir));
         printf("%d\n",(*raiz)->dado);
     }
+}
+
+
+//---------------------------
+
+//----------Rotações---------
+
+// As rotações são aplicadas no ancestral mais próximo do nó inserido
+// cujo fator de balanceamento passa a ser +2 ou -2.
+// As funções abaixo, portanto, recebem esse ancestral, que corresponde
+// à raiz da sub-árvore a ser rotacionada.
+
+
+// A partir da raiz da sub-árvore:
+// direita do filho da esquerda se torna a sua esquerda. 
+// direita do filho da esquerda recebe a raiz. 
+// filho da esquerda se torna raiz. 
+void RotacaoLL(Arvore *raiz){
+    struct No *no;
+    no = (*raiz)->esq;
+    (*raiz)->esq = no->dir;
+    no->dir = *raiz;
+    (*raiz)->altura = maior(altura_No((*raiz)->esq),altura_No((*raiz)->dir)) + 1;
+    no->altura = maior(altura_No(no->esq),(*raiz)->altura) + 1;
+    *raiz = no;
+}
+
+// A partir da raiz da sub-árvore:
+// esquerda do filho da direita se torna a sua direita. 
+// esquerda do filho da direita recebe a raiz. 
+// filho da direita se torna raiz. 
+void RotacaoRR(Arvore *raiz){
+    struct No *no;
+    no = (*raiz)->dir;
+    (*raiz)->dir = no->esq;
+    no->esq = (*raiz);
+    (*raiz)->altura = maior(altura_No((*raiz)->esq),altura_No((*raiz)->dir)) + 1;
+    no->altura = maior(altura_No(no->dir),(*raiz)->altura) + 1;
+    (*raiz) = no;
+}
+
+// rotação à esquerda no filho da esquerda; rotação à direta na raiz
+void RotacaoLR(Arvore *raiz){
+    RotacaoRR(&(*raiz)->esq);
+    RotacaoLL(raiz);
+}
+
+// rotação à direita no filho da direita; rotação à esquerda na raiz
+void RotacaoRL(Arvore *raiz){
+    RotacaoLL(&(*raiz)->dir);
+    RotacaoRR(raiz);
+}
+
+
+//---------------------------
+
+int insere_Arvore(Arvore *raiz, int valor){
+
+    int res;    // variável para indicar o sucesso da inserção
+
+    // árvore vazia ou nó folha
+    if(*raiz == NULL){
+        struct No *novo;
+        novo = (struct No*)malloc(sizeof(struct No));
+        if(novo == NULL)
+            return 0;
+
+        novo->dado = valor;
+        novo->altura = 0;
+        novo->esq = NULL;
+        novo->dir = NULL;
+        *raiz = novo;
+        return 1;
+    }
+
+    // para esquerda
+    struct No *atual = *raiz;
+    if(valor < atual->dado){
+        if((res = insere_Arvore(&(atual->esq), valor)) == 1){
+            if(fatorBalanceamento_No(atual) >= 2){
+                if(valor < (*raiz)->esq->dado ){    // esquerda
+                    RotacaoLL(raiz);
+                }else{  // direita
+                    RotacaoLR(raiz);
+                }
+            }
+        }
+    }else{
+        // para direita
+        if(valor > atual->dado){
+            if((res = insere_Arvore(&(atual->dir), valor)) == 1){
+                if(fatorBalanceamento_No(atual) >= 2){
+                    if((*raiz)->dir->dado < valor){     // direita
+                        RotacaoRR(raiz);
+                    }else{  // esquerda
+                        RotacaoRL(raiz);
+                    }
+                }
+            }
+        }else{
+            printf("Valor duplicado!!\n");
+            return 0;
+        }
+    }
+
+    // atualiza a altura do nó
+    atual->altura = maior(altura_No(atual->esq),altura_No(atual->dir)) + 1;
+
+    return res;
+}
+
+// função auxiliar caso o nó removido tenha 2 filhos.
+// procura o elemento mais à esquerda da árvore da direita para substituir o nó removido
+struct No* procuraMenor(struct No* atual){
+    struct No *no1 = atual;
+    struct No *no2 = atual->esq;
+    while(no2 != NULL){
+        no1 = no2;
+        no2 = no2->esq;
+    }
+    return no1;
+}
+
+int remove_Arvore(Arvore *raiz, int valor){
+	if(*raiz == NULL){
+	    printf("Valor não existe!!\n");
+	    return 0;
+	}
+
+    int res;
+
+    // se o valor for menor que o valor da raiz, prosegue para a árvore da esquerda
+	if(valor < (*raiz)->dado){
+        // quando a remoção aí acontece
+	    if((res = remove_Arvore(&(*raiz)->esq,valor)) == 1){
+            if(fatorBalanceamento_No(*raiz) >= 2){  // se desbalanceada, testa a altura da sub-árvore da direita
+                if(altura_No((*raiz)->dir->esq) <= altura_No((*raiz)->dir->dir))
+                    RotacaoRR(raiz);
+                else
+                    RotacaoRL(raiz);
+            }
+	    }
+	}
+
+    // se o valor for menor que o maior da raiz, prosegue para a árvore da direita
+	if((*raiz)->dado < valor){
+        // quando a remoção aí acontece
+	    if((res = remove_Arvore(&(*raiz)->dir, valor)) == 1){
+            if(fatorBalanceamento_No(*raiz) >= 2){  // se desbalanceada, testa a altura da sub-árvore da esquerda
+                if(altura_No((*raiz)->esq->dir) <= altura_No((*raiz)->esq->esq) )
+                    RotacaoLL(raiz);
+                else
+                    RotacaoLR(raiz);
+            }
+	    }
+	}
+
+    // encontrou o valor
+	if((*raiz)->dado == valor){
+        // nó tem 1 filho ou nenhum
+        // 1 filho: o filho substitui o nó
+	    if(((*raiz)->esq == NULL || (*raiz)->dir == NULL)){
+			struct No *oldNode = (*raiz);
+			if((*raiz)->esq != NULL)
+                *raiz = (*raiz)->esq;
+            else
+                *raiz = (*raiz)->dir;
+			free(oldNode);
+
+		// nó tem 2 filhos
+        }else {
+            // procura a valor mais à esquerda (menor valor) da sub-ávore da direita
+            // para substituir o nó removido
+			struct No* temp = procuraMenor((*raiz)->dir);
+			(*raiz)->dado = temp->dado;
+			remove_Arvore(&(*raiz)->dir, (*raiz)->dado);
+            if(fatorBalanceamento_No(*raiz) >= 2){
+				if(altura_No((*raiz)->esq->dir) <= altura_No((*raiz)->esq->esq))
+					RotacaoLL(raiz);
+				else
+					RotacaoLR(raiz);
+			}
+		}
+		if (*raiz != NULL)
+            (*raiz)->altura = maior(altura_No((*raiz)->esq),altura_No((*raiz)->dir)) + 1;
+		return 1;
+	}
+
+    // atualiza a altura da raiz
+	(*raiz)->altura = maior(altura_No((*raiz)->esq),altura_No((*raiz)->dir)) + 1;
+
+	return res;
 }
 
 void main(){
